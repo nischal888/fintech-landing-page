449c62d187cdebb0bd9e9ad9f142616528bd20fa
Nischal <nischalrajbudhathoki@gmail.com>
2025-06-15 16:47:57 +0300

    add gh-pages and configure

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,7 @@ const __dirname = path.dirname(__filename);
 // https://vite.dev/config/
 export default defineConfig({
 	plugins: [react()],
+	base: '/fintech-landing-page',
 	resolve: {
 		alias: {
 			'@': path.resolve(__dirname, './src'),
